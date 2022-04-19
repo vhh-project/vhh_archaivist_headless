e22b4f82a9d760a6511b494254d32de4b24ba090
David Banyasz <1948523+dbanyasz@users.noreply.github.com>
2022-04-19 15:53:03 +0200

    Change baseline docker paths

--- baseline_vespa_app/launch-vespa.sh
+++ baseline_vespa_app/launch-vespa.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # wait for vespa config server to launch and launch content server in the background
-/wait-for-config-server.sh &
+/baseline/wait-for-config-server.sh &
 
 # container-local script for launching entry-point
 /usr/local/bin/start-container.sh
